8853a5821abdf6525cd11ed18986f5964b2a62f5
= <=>
2023-06-14 12:30:33 +0700

    🐱‍🐉 Update authentication service and application security config

---
+++ src/main/java/com/ticket/server/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.ticket.server.controller;
+
+import com.ticket.server.model.User;
+import com.ticket.server.model.UserRegister;
+import com.ticket.server.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/user")
+@AllArgsConstructor
+public class UserController {
+    private UserService userService;
+
+    public User register(@RequestBody UserRegister userRegister){
+        return userService.register(userRegister);
+    }
+}
