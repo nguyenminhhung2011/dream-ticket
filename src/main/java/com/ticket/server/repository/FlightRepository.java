3204f59c58b3fde81815c0a445ea75ca5f0473c9
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2023-06-19 09:39:13 +0700

    🐼Update logic

--- src/main/java/com/ticket/server/repository/FlightRepository.java
+++ src/main/java/com/ticket/server/repository/FlightRepository.java
@@ -34,13 +34,16 @@ public interface FlightRepository extends JpaRepository<Flight, Long> {
             FROM flight AS f
             JOIN airport AS a1 ON f.arrival_airport_id = a1.id
             	where a1.id = :id
+            ORDER BY f.arrival_time
             """, nativeQuery = true)
     List<Flight> getFlightWithArrivalId(Integer id);
+
     @Query(value = """
             SELECT f.*
             FROM flight AS f
             JOIN airport AS a1 ON f.departure_airport_id = a1.id
             	where a1.id = :id
+            ORDER BY f.departure_airport_id
             """, nativeQuery = true)
     List<Flight> getFlightDepartureId(Integer id);
 }
