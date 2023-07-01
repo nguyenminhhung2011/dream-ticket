85b195e57709659014694c399947a5d003000188
hoangankin2211 <truonghuynhduchoang@gmail.com>
2023-07-01 23:03:26 +0700

    Deploy: v1

--- src/main/java/com/ticket/server/config/SecurityConfiguration.java
+++ src/main/java/com/ticket/server/config/SecurityConfiguration.java
@@ -23,6 +23,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -81,5 +87,16 @@ public class SecurityConfiguration {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        System.out.println("corsConfigurationSource");
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
